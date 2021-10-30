bdd84e03b502d1c70b4f734a25542db1d6af5ca1
orang3tea <majkonikolaj2@gmail.com>
2021-10-30 03:33:06 +0300

    dsrery

--- src/sample/controllers/MainWindowController.java
+++ src/sample/controllers/MainWindowController.java
@@ -26,9 +26,7 @@ public class MainWindowController {
 
     @FXML
     private Button wallButton;
-    //jjholjn
-    //dummy
-    //dummmyyy
+
     public void toSceneWall() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample/fxml/WallWindow.fxml"));
         Stage window =(Stage) wallButton.getScene().getWindow();
