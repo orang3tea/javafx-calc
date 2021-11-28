3b4f59fbdbdff5451171b182e52fda34b0a36f94
orang3tea <majkonikolaj2@gmail.com>
2021-11-28 16:35:27 +0300

    It contains  created methods in "Plaster" and "Calculations".

--- src/sample/controllers/forscene/calculator/Calculations.java
+++ src/sample/controllers/forscene/calculator/Calculations.java
@@ -9,7 +9,14 @@ public class Calculations extends ControllerAction {
 
     private static final BigDecimal CEN_IN_MET_SQ_CONVERT_FACTOR = BigDecimal.valueOf(10000);
     private static final int BOX_AREA_CONV_FACTOR = 100;
+    private static final BigDecimal CEN_SQ_TO_MET_CUB = BigDecimal.valueOf(1000);
     public static final String ERROR_MESSAGE = "Не все поля заполнены!";
+
+    private static final BigDecimal CONCRETE_PLASTER_DENSITY = BigDecimal.valueOf(1800);
+    private static final BigDecimal GYPSUM_PLASTER_DENSITY = BigDecimal.valueOf(900);
+    private static final BigDecimal START_PUTTY_DENSITY  = BigDecimal.valueOf(1200);
+    private static final BigDecimal FINISH_PUTTY_DENSITY = BigDecimal.valueOf(800);
+
 
 
     protected String countInMeters(int length, int width) {
@@ -45,6 +52,25 @@ public class Calculations extends ControllerAction {
 
         return bDWallsArea.multiply(CEN_IN_MET_SQ_CONVERT_FACTOR).divide(bDPackArea, 0, RoundingMode.CEILING)
                 .toString();
+    }
+
+    protected String countMaterialsVolume(String wallsArea, String depth) {
+        BigDecimal bDWallsArea = new BigDecimal(wallsArea);
+        BigDecimal bDDepth = new BigDecimal(depth);
+        return bDWallsArea.multiply(bDDepth).divide(CEN_SQ_TO_MET_CUB, 1, RoundingMode.CEILING).toString();
+    }
+
+    protected String countMaterialWeight(String volume, String material) {
+        BigDecimal materialDensity = null;
+        BigDecimal materialVolume = new BigDecimal(volume);
+        switch (material) {
+            case "цем-я штукатурка" -> materialDensity = CONCRETE_PLASTER_DENSITY;
+            case "гипс-я штукатурка" -> materialDensity = GYPSUM_PLASTER_DENSITY;
+            case "шпатлевка старт" -> materialDensity = START_PUTTY_DENSITY;
+            case "шпатлевка финиш" -> materialDensity = FINISH_PUTTY_DENSITY;
+        }
+        assert materialDensity != null;
+        return materialDensity.multiply(materialVolume).toString();
     }
 
     protected int countPackArea(int length, int width) {
--- src/sample/controllers/forscene/calculator/Plaster.java
+++ src/sample/controllers/forscene/calculator/Plaster.java
@@ -1,15 +1,33 @@
 package sample.controllers.forscene.calculator;
 
 import java.net.URL;
+import javafx.scene.control.ChoiceBox;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import sample.controllers.ControllerAction;
 import sample.controllers.Paths;
 
-public class Plaster extends ControllerAction {
+public class Plaster extends Calculations {
+    @FXML
+    private static final String[] variantsFofChoiceBox = {"цем-я штукатурка","гипс-я штукатурка",
+            "шпатлевка старт", "шпатлевка финиш"};
+
+    static private TextField[] allTextFields;
+
+    @FXML
+    private ChoiceBox<String> materials;
+
+    @FXML
+    private Label resultLabelBottom;
+
+    @FXML
+    private Label resultLabelTop;
 
     @FXML
     private ResourceBundle resources;
@@ -21,7 +39,31 @@ public class Plaster extends ControllerAction {
     private Button ResultButton;
 
     @FXML
+    private TextField coatingThickness;
+
+    @FXML
     private ImageView goBack;
+
+    @FXML
+    private TextField heightCentimeters;
+
+    @FXML
+    private TextField heightMeters;
+
+    @FXML
+    private TextField lengthCentimeters;
+
+    @FXML
+    private TextField lengthMeters;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private TextField widthCentimeters;
+
+    @FXML
+    private TextField widthMeters;
 
     @FXML
     void goBack(MouseEvent event) {
@@ -33,6 +75,39 @@ public class Plaster extends ControllerAction {
         assert ResultButton != null : "fx:id=\"ResultButton\" was not injected: check your FXML file 'plaster.fxml'.";
         assert goBack != null : "fx:id=\"goBack\" was not injected: check your FXML file 'plaster.fxml'.";
 
+        allTextFields = new TextField[] {lengthMeters, lengthCentimeters, widthMeters, widthCentimeters,
+                heightMeters, heightCentimeters, coatingThickness};
+        typeCheck(allTextFields);
+
+        materials.getItems().addAll(variantsFofChoiceBox);
+        materials.setValue(variantsFofChoiceBox[0]);
+
     }
+
+    @FXML
+    void calculate(ActionEvent event) {
+        int length;
+        int width;
+        int height;
+        String depth;
+        String materialsVolume;
+
+        if(checkFields(allTextFields)) {
+            length = asOneNumber(lengthMeters, lengthCentimeters);
+            width = asOneNumber(widthMeters, widthCentimeters);
+            height = asOneNumber(heightMeters, heightCentimeters);
+            depth = coatingThickness.getText();
+            materialsVolume = countMaterialsVolume(wallsArea(length, width, height), depth);
+
+            resultLabelTop.setText("объем готовой смеси, м.куб    " + materialsVolume);
+            resultLabelBottom.setText("вес готовой смеси, кг    " + countMaterialWeight(materialsVolume, materials.getValue()));
+
+        }
+        else
+            resultLabelTop.setText(ERROR_MESSAGE);
+
+
+    }
+
 
 }
